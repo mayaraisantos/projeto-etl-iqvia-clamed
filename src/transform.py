55c6bea648b5f63fcd0397043ec1de50a55b9e0d
mayaraisantos <mayara_i_santos@estudante.sesisenai.org.br>
2025-12-23 18:36:23 -0300

    logs na etapa transform

--- src/transform.py
+++ src/transform.py
@@ -1,25 +1,25 @@
 import pandas as pd
 
 def transform_data(vendas, filial):
-    # padronização de colunas
     vendas.columns = vendas.columns.str.strip().str.lower().str.replace(" ", "_")
     filial.columns = filial.columns.str.strip().str.lower().str.replace(" ", "_")
 
-    # tipos
     vendas['ean'] = vendas['ean'].astype(str)
-
-    # tratamento de nulos
     vendas = vendas.fillna(0)
-
-    # remoção de duplicatas
     vendas = vendas.drop_duplicates()
 
-    # colunas derivadas
     vendas['mes_ano'] = pd.to_datetime(vendas['mes_ano'], format='%Y-%m')
     vendas['participacao_percentual'] = vendas['participacao_clamed'] * 100
 
-    # merge
     df_final = pd.merge(vendas, filial, on="brick_id", how="left")
 
+    print("Transformação concluída:")
+    print("- Padronização de colunas")
+    print("- Tratamento de nulos")
+    print("- Remoção de duplicatas")
+    print("- Criação de colunas derivadas")
+    print(f"- Total final de registros: {df_final.shape[0]}")
+
     return df_final
+
 
