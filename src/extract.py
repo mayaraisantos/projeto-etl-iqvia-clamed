555c888fd384d3183371d0c6bb9d02679f09adad
mayaraisantos <mayara_i_santos@estudante.sesisenai.org.br>
2025-12-23 18:35:43 -0300

    logs na etapa extract
    Added print statements to confirm data extraction counts.

--- src/extract.py
+++ src/extract.py
@@ -3,5 +3,9 @@ import pandas as pd
 def extract_data():
     vendas = pd.read_excel("data/MS_12_2022_sample.xlsx")
     filial = pd.read_excel("data/filial-brick_sample.xlsx")
+
+    print(f"Extração concluída: {vendas.shape[0]} registros de vendas")
+    print(f"Extração concluída: {filial.shape[0]} registros de filiais")
+
     return vendas, filial
 
