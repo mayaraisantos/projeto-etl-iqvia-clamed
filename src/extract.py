5e8b2977cf7e956d536dc2fae2246f863df989d7
mayaraisantos <mayara_i_santos@estudante.sesisenai.org.br>
2025-12-23 18:24:38 -0300

    Update extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,1 +1,7 @@
-# arquivo extract.py
+import pandas as pd
+
+def extract_data():
+    vendas = pd.read_excel("data/MS_12_2022_sample.xlsx")
+    filial = pd.read_excel("data/filial-brick_sample.xlsx")
+    return vendas, filial
+
