c179ad00a2e604afbd1f9dce3fcfe0386f6cb811
mayaraisantos <mayara_i_santos@estudante.sesisenai.org.br>
2025-12-23 18:37:13 -0300

    logs no pipeline principal

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from extract import extract_data
 from transform import transform_data
 from load import load_data
 
+print("Iniciando pipeline ETL...")
+
 vendas, filial = extract_data()
 df_final = transform_data(vendas, filial)
 load_data(df_final)
+
+print("Pipeline ETL finalizado com sucesso")
