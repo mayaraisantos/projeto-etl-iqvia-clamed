df442828b8e7f4caf65f3bd3fd6b3b4fedf21245
mayaraisantos <mayara_i_santos@estudante.sesisenai.org.br>
2025-12-23 18:27:15 -0300

    conexao postgres e load
    Added a function to load data into a PostgreSQL database.

--- src/load.py
+++ src/load.py
@@ -1,1 +1,14 @@
-# arquivo load.py
+from sqlalchemy import create_engine
+
+def load_data(df):
+    engine = create_engine(
+        "postgresql+psycopg2://usuario:senha@localhost:5432/seu_banco"
+    )
+
+    df.to_sql(
+        "fact_vendas_iqvia",
+        engine,
+        if_exists="replace",
+        index=False
+    )
+
